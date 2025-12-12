a7712b530435ca8ac35eab99d91f457c85965b4a
Harish <harishmurugan8503@gmail.com>
2025-12-12 22:53:59 +0530

    Implement fixed array initialization and manipulation

--- day_2_array/Fixed_Array/static_arr.sv
+++ day_2_array/Fixed_Array/static_arr.sv
@@ -1,1 +1,26 @@
+module array ;
+  //array size declaration
+  logic [7:0] fixed_arr [8];
+  //size is 8 elelement of each withn 8 bits
+  
+  initial begin
+  fixed_arr ='{1,2,3,4,5,6,7,8};
+  $display ("arr:%p",fixed_arr);
+    
+    fixed_arr[5]=10;
+     $display ("arr:%p",fixed_arr);
+    
+    fixed_arr[5][1]=1'b0;
+    $display ("arr:%b",fixed_arr[5]);
+    
+    $display ("arr:%p",fixed_arr);
+    end
+endmodule
 
+
+
+//output
+arr:'{'h1, 'h2, 'h3, 'h4, 'h5, 'h6, 'h7, 'h8} 
+arr:'{'h1, 'h2, 'h3, 'h4, 'h5, 'ha, 'h7, 'h8} 
+arr:00001000
+arr:'{'h1, 'h2, 'h3, 'h4, 'h5, 'h8, 'h7, 'h8} 
