ea8e511b70529f7053ca8be5f66890d05026a7eb
Harish <harishmurugan8503@gmail.com>
2026-01-25 22:53:14 +0530

    Add output samples
    Add output samples for mailbox communication transactions.

---
+++ day_5/mailbox_communication/5_outputsamples.sv
@@ -0,0 +1,5 @@
+# transaction object get fro  driver a=000,b=0
+# transaction object get fro  driver a=001,b=0
+# transaction object get fro  driver a=100,b=1
+# transaction object get fro  driver a=100,b=0
+# transaction object get fro  driver a=111,b=1
