b9c83647aee799f5aa8497bbebe666eccf300e86
Harish <harishmurugan8503@gmail.com>
2026-02-08 00:32:51 +0530

    arr_pattern_const.sv

---
+++ Constraint_assignment/03_iteration_constraint/4_arr_pattern_const(145145).sv
@@ -0,0 +1,33 @@
+//pattern -122112211--->1221 1221
+
+class arr_const;
+  
+  rand int dyn[];
+  //local 
+  int pattern[3]='{1,4,5};
+  
+  constraint size_c{dyn.size() inside {[5:15]};}
+  
+  constraint order_c{
+    foreach (dyn[i])
+      dyn[i]==pattern[i%3];}
+    
+endclass
+
+module pattern_const;
+  arr_const c;
+  initial begin
+    c=new();
+    repeat(3)begin
+      c.randomize();
+      $display("%p",c);
+    end
+  end
+endmodule
+    
+  
+  
+  
+      
+      
+                
